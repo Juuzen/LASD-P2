4db97c07f804d04f65ead7f2c116323c86eeb950
Salvatore <backsofangels@gmail.com>
2021-05-19 23:59:01 +0200

    Emptied main for clarity

--- main.c
+++ main.c
@@ -2,15 +2,5 @@
 #include "productCatalogue.h"
 
 int main() {
-
-    PtrCatalogue catalogue = NULL;
-
-    catalogue = retrieveItemsFromCatalogueFile("catalogue.txt");
-    if(catalogue==NULL){
-        printf("errore");
-    }
-
-    print(catalogue);
-
     return 0;
 }
