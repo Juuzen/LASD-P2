01c3f48851fb58abf63d6cea0fc409d4457e9fc9
Salvatore <backsofangels@gmail.com>
2021-05-20 22:47:01 +0200

    Create function to create order and calculate weight

---
+++ orderManagement.c
@@ -0,0 +1,40 @@
+//
+// Created by backs on 20/05/2021.
+//
+
+#include "stdio.h"
+#include "orderManagement.h"
+
+PtrOrder createNewOrder(Item item, int quantity) {
+    PtrOrder order;
+    order->item = item;
+    order->quantity = quantity;
+    order->next = NULL;
+    return order;
+}
+
+PtrOrder insertOrderOnEnd(PtrOrder head, PtrOrder order) {
+    if (head == NULL) {
+        return order;
+    }
+
+    head->next = insertOrderOnEnd(head->next, order);
+    return head;
+}
+
+int calculateOrderWeight(PtrOrder head) {
+    if (head == NULL) {
+        return 0;
+    } else {
+        return head->quantity + calculateOrderWeight(head->next);
+    }
+}
+
+void printOrderList(PtrOrder head) {
+    if (head == NULL) {
+        return;
+    }
+
+    printf("%s\t%d\t%d\n", head->item.itemLabel, head->item.specificWeight, head->quantity);
+    printOrderList(head->next);
+}
